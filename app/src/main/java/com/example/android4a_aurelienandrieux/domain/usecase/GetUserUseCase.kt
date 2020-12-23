46823ed97ca7b8c9c757aabcca032d003cdae956
Blex <callenoni@gmail.com>
2020-12-23 16:09:42 +0100

    Implementation of a notion of login

--- app/src/main/java/com/example/android4a_aurelienandrieux/domain/usecase/GetUserUseCase.kt
+++ app/src/main/java/com/example/android4a_aurelienandrieux/domain/usecase/GetUserUseCase.kt
@@ -6,7 +6,7 @@ import com.example.android4a_aurelienandrieux.domain.entity.User
 class GetUserUseCase(
     private val userRepository: UserRepository
 ) {
-    suspend fun invoke(email: String): User {
+    suspend fun invoke(email: String): User? {
         return userRepository.getUser(email)
     }
 }
--- app/src/main/java/com/example/android4a_aurelienandrieux/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/android4a_aurelienandrieux/presentation/main/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.example.android4a_aurelienandrieux.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.koin.android.ext.android.inject
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -37,11 +38,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_button.setOnClickListener{
-            mainViewModel.onClickedIncrement("")
-        }
-        mainViewModel.counter.observe(this, Observer {
-            main_text.text = it.toString()
+        mainViewModel.loginLiveData.observe(this, Observer{
+            when(it){
+                is LoginSuccess -> TODO()
+                LoginError -> {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle("Erreur")
+                        .setMessage("Compte Inconnu")
+                        .setPositiveButton("Ok") { dialog, which ->
+                            dialog.dismiss()
+                        }
+                        .show()
+                }
+            }
         })
+        login_button.setOnClickListener{
+            mainViewModel.onClickedLogin(login_edit.text.toString().trim(), password_edit.text.toString())
+        }
     }
 }
